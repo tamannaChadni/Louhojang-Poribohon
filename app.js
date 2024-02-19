efa6a657301e68d9b6b75c80adcc29e37831de2b
Tamanna Chadni <tamanna.chadni.eng@gmail.com>
2024-02-19 19:36:41 +0600

    functional work continue

--- app.js
+++ app.js
@@ -4,63 +4,82 @@ function mainSection() {
     .scrollIntoView({ behavior: "smooth" });
 }
 
-// function modal() {
-//   const modalBox = document.getElementById("next");
-//   modalBox.showModal();
-
-// }
- let serial = 0; 
- let totalPrice = 0;
+let totalPrice = 0;
+let gradTotal = 0;
 
 const seatNumber = document.getElementById("seat-number");
 
 seatNumber.addEventListener("click", (event) => {
   if (event.target.tagName === "BUTTON") {
-    console.log("button was clicked");
-    // disable and change the button color
-    event.target.classList.add("disabled:bg-lime-300");
-    event.target.disabled = true;
+    // console.log("button was clicked");
+    const seatBtn = event.target;
 
+    // showing booked seat details
+    const addSeatEl = document.getElementById("add-seat");
+    let totalBookedSeat = parseInt(addSeatEl.innerText);
+    if (totalBookedSeat > 3) {
+      alert("You can not book more than 4 seats");
+      return;
+    }
+    totalBookedSeat++;
+    addSeatEl.innerText = totalBookedSeat;
+
+    // disable and change the button color
+    seatBtn.classList.add("disabled:bg-lime-300");
+    seatBtn.disabled = true;
+
+    // Show remaiing seats
     const availableSeatEl = document.getElementById("seat-available");
     const remainingSeat = availableSeatEl.innerText - 1;
     availableSeatEl.innerText = remainingSeat;
 
-    const addSeat = document.getElementById("add-seat");
-    const bookedSeat = addSeat.innerText;
-    const currentSeat = parseInt(bookedSeat);
-    const fixSeat = currentSeat + 1;
-    addSeat.innerText = fixSeat;
+    // const seatName = seatNumber.querySelector("button").innerText;
+    // const seatName = document.getElementById("seat-number").innerText;
+    // console.log(seatName);
 
+    const classSeat = "Economic class";
+    const price = parseInt(550);
 
-    const table = document.getElementById("table");
+    const seatDetails = document.getElementById("seat-details");
+    const p = document.createElement("p");
+    p.innerText = seatBtn.innerText + " " + classSeat + " " + price;
+    seatDetails.appendChild(p);
 
-    for (let index = 0; index < seatNumber.length; index++) {
-        const number = seatNumber[index];
+    // calculate price
+    totalPrice += price;
+    gradTotal += price;
 
-    const row = number.createElement("tr");
+    document.getElementById("total-price").innerText = totalPrice;
+    document.getElementById("grand-price").innerText = gradTotal;
+  }
+});
 
-    const title = seatNumber.innerText;
-    const staticTh = number.createElement('th');
-    staticTh.innerText = serial++;
-    const dynamicTd = number.createElement("td");
-        dynamicTd.textContent = title;
+const applyBtn = document.getElementById("btn-apply");
+applyBtn.addEventListener("click", function () {
+  // console.log("clicked");
+  const cuponElement = document.getElementById("cupon-field").value;
+  // console.log(cuponElement);
 
-        const staticTd1 = number.createElement("td");
-        staticTd1.innerText = "Economic class";
+  if (cuponElement === 'NEW15') {
+    const discountAmount = totalPrice * 0.15;
+    const grandPrice = document.getElementById("grand-price");
+    const totalAmount = totalPrice - discountAmount;
+    grandPrice.innerText = totalAmount;
+    document.getElementById("cupon-field").value = "";
 
-        const staticTd2 = number.createElement("td");
-        parseInt(staticTd2.innerText)  = 500;
+    const cuponSection = document.getElementById('cupon-section');
+    cuponSection.classList.add('hidden');
 
-        row.appendChild(staticTh);
-        row.appendChild(dynamicTd);
-        row.appendChild(staticTd1);
-        row.appendChild(staticTd2);
-        table.appendChild(row);
-        // console.log(row);
-    }
-
-    // console.log(availableSeat);
-
-    // decrease seat count
+  } 
+  else if (cuponElement === 'Couple 20') {
+    const discountAmount = totalPrice * 0.2;
+    const grandPrice = document.getElementById("grand-price");
+    const totalAmount = totalPrice - discountAmount;
+    grandPrice.innerText = totalAmount;
+    document.getElementById("cupon-field").value = "";
+  } 
+  else {
+    alert("Invaild cupon");
+    document.getElementById("cupon-field").value = "";
   }
 });
